526ae2b300132eb303bd78131c7c691181233fff
Rui | 罗尔 <66991791+qrovic@users.noreply.github.com>
2024-01-27 07:41:52 +0800

    updated sql

--- se2 old.sql
+++ se2 old.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 27, 2024 at 12:40 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost
+-- Generation Time: Jan 26, 2024 at 05:38 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,42 +42,8 @@ CREATE TABLE `item` (
   `storeid` int(11) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `category` varchar(255) DEFAULT NULL,
-  `pic` varchar(255) DEFAULT NULL
+  `pic` blob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `item`
---
-
-INSERT INTO `item` (`id`, `storeid`, `name`, `category`, `pic`) VALUES
-(2, 38, 'Mocha Latte', 'Drinks', 'mocha.jpeg'),
-(3, 38, 'Fries', 'Snacks', 'mcdofries.jpg'),
-(4, 38, 'Iced Coffee', 'Combos', 'icedcofee.jpg'),
-(5, 38, 'Spaghetti', 'Snacks', 'spag.jpeg'),
-(6, 40, 'Burger', 'Snacks', 'burger.jpeg'),
-(12, 49, 'Fries', 'Snacks', 'jollibee-french-fries-021419-1.jpg'),
-(13, 49, 'Chicken Joy', 'Meals', 'a2002470507_10.jpg'),
-(14, 49, 'Coke Float', 'Drinks', 'afcba0789b6c1e945b7e93cf6b6e6efc.jpg'),
-(15, 49, 'Float with Fries', 'Combos', '22219996_1592256907487196_8596183077658277485_o.jpg'),
-(16, 46, 'Legs with Rice', 'Combos', 'mang-inasal-ready-to-cook-01-2.jpg'),
-(17, 46, 'Halo Halo', 'Desserts', 'a1yd4IAL-MKMVhTqpzfERg.jpg'),
-(18, 46, 'Palabok w halo', 'Combos', 'Palabok-Halo-Halo-Merienda-Combo.jpg'),
-(19, 38, 'Chicken', 'Meals', '2pcChickenRice-500.jpeg'),
-(20, 38, 'Ube sundae', 'Desserts', 'mcdo-1-hot-fudge-sundae-1606112816.jpeg'),
-(21, 38, 'Choco Sundae', 'Desserts', 'mcdo-1-hot-fudge-sundae-1606112816.jpeg'),
-(22, 38, 'Strawberry Float', 'Drinks', 'Red-Velvet-McFloat.webp'),
-(23, 38, 'Float w fries', 'Combos', 'download (1).jpg'),
-(24, 38, 'Royal Float', 'Drinks', '6047_ouT40HgTUVKbk1bm_.jpg'),
-(25, 38, 'Mango Shake', 'Drinks', 'DC_201906_2790_MediumMangoPineappleSmoothie_Glass_A1_832x472_1-3-product-tile-desktop.jpg'),
-(26, 38, 'Foamy Caramel', 'Drinks', '04d5e0a61a03f43d4136c336aaa0cc98.png'),
-(27, 38, 'Iced Coffee', 'Drinks', 'icedcaramelmac_2col_2-column-desktop.jpg'),
-(28, 38, 'Caramel Latte', 'Drinks', 't-mcdonalds-mccafe-iced-mocha-medium-1566921962.jpg'),
-(30, 38, 'Fries Chicken Coke', 'Combos', 'artworks-JHayJ4ei7u8FDVl7-hsS2fg-t500x500.jpg'),
-(31, 38, 'Mc cafe chicken', 'Combos', 'bfcaa72dc815c9309c595d85343f226d.png_720x720q80.png'),
-(32, 38, 'Breakfast bundle', 'Combos', 'greenprc-sausagemcmuffincoffeehb435x320-1587617068.png'),
-(33, 38, 'Oreo mcflurry', 'Desserts', 'screen-shot-2019-08-26-at-1-02-16-pm-1566838950.png'),
-(34, 38, 'Mango pie', 'Desserts', '13987665_10157244563490023_1444107325514963127_o.jpg'),
-(35, 38, 'Choco Pie', 'Desserts', '0619eb8ec27c02b0a49395bb3ed67d2c.jpg');
 
 -- --------------------------------------------------------
 
@@ -174,14 +140,7 @@ INSERT INTO `staff` (`id`, `storeid`, `fname`, `lname`, `contactno`, `email`, `p
 (28, 40, 'Heuhsudhbsd', 'shijfsrjhksrf', '09193162290', 'febijbkjsfesk@gmail.com', 'fhjkbscdnjkcsdknjscd', 'Owner', 'Jbjscbkjc'),
 (29, 41, 'DADASH', 'FSHHDF', '09191873187', 'admin@gmail.com', 'admin', 'Owner', 'SDFHSH'),
 (30, 42, 'jjsjsjksf', '', '', '', '', 'Owner', ''),
-(31, 43, 'sdhsjsdj', 'DXVJDJ', '2898938', 'admin@gmail.com', 'admin', 'Owner', 'DXVJJN'),
-(32, 44, NULL, '', '', '', '', 'Owner', ''),
-(33, 45, NULL, '', '', '', '', 'Owner', ''),
-(34, 46, 'Chicken inasal', 'Inasal', '09193162888', 'mangjose@gmail.com', 'manginasal', 'Owner', 'Jose'),
-(35, 47, 'yummy donut', 'donut', '091942622284', 'dunkin@gmail.com', 'dunkin', 'Owner', ''),
-(36, 48, 'esdhjf', 'donut', '092327837', 'dunkin2@gmail.com', 'dunkin', 'Owner', ''),
-(37, 49, 'jasdjkjda', 'bee', '09193162290', 'jollibee@gmail.com', 'dunkin', 'Owner', ''),
-(38, 50, 'fgdfg', '', '455', 'dunkin@gmail.com', 'dunkin', 'Owner', '');
+(31, 43, 'sdhsjsdj', 'DXVJDJ', '2898938', 'admin@gmail.com', 'admin', 'Owner', 'DXVJJN');
 
 -- --------------------------------------------------------
 
@@ -193,7 +152,7 @@ CREATE TABLE `store` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT 'Online',
+  `status` varchar(255) DEFAULT 'offline',
   `pic` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -202,11 +161,12 @@ CREATE TABLE `store` (
 --
 
 INSERT INTO `store` (`id`, `name`, `description`, `status`, `pic`) VALUES
-(38, 'Mcdo', 'low high hHAHAHHA', 'Online', 'mcdo.png'),
-(46, 'Mang Inasal', 'Chicken inasal', 'Online', 'flat,750x,075,f-pad,750x1000,f8f8f8 (1).jpg'),
-(47, 'Dunkin', 'yummy donut', 'Online', '1678.png'),
-(49, 'Jollibee', 'jasdjkjda', 'Online', '87d166c686ccca056ebcafba8ceae13f.jpg'),
-(50, 'sfds', 'fgdfg', 'Online', '—Pngtree—vector shopping bag icon_4162833.png');
+(38, 'Mcdo', 'low high hHAHAHHA', '', 'se (3).sql'),
+(39, 'Rouel Cafe', 'Where every drink turns into a dream', '', ''),
+(40, 'Mcdo', 'Heuhsudhbsd', '', 'IMG_0361.jpeg'),
+(41, 'HUHU', 'DADASH', '', 'img-worlds-of-adventure.jpg'),
+(42, 'sbhdshbh', 'jjsjsjksf', 'active', 'img-worlds-of-adventure.jpg'),
+(43, 'hdedfsjkaedfhcbsmanjdgwyeb', 'sdhsjsdj', '', 'img-worlds-of-adventure.jpg');
 
 -- --------------------------------------------------------
 
@@ -307,7 +267,7 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `itemprice`
@@ -343,13 +303,13 @@ ALTER TABLE `sale`
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `store`
 --
 ALTER TABLE `store`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `user`
